48c83f931436c0fb739c7327faf72cdc777739a0
Dharshanaa SR <dharshanaasr04@gmail.com>
2026-06-17 19:56:27 +0530

    vercerl json update

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on", PORT));
+
 // Routes
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/orders", require("./routes/orderRoutes"));
-app.use("/api/cart", cartRoutes);
-app.use("/api/admin", require("./routes/adminRoutes"));
 
 // Health check
 app.get("/", (req, res) => {
@@ -24,9 +22,12 @@ app.get("/", (req, res) => {
 
 // DB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI || "mongodb://dharshana:dharshana@ac-mvp7ebm-shard-00-00.sj4ak4j.mongodb.net:27017,ac-mvp7ebm-shard-00-01.sj4ak4j.mongodb.net:27017,ac-mvp7ebm-shard-00-02.sj4ak4j.mongodb.net:27017/?ssl=true&replicaSet=atlas-hg4y9f-shard-0&authSource=admin&appName=Cluster0")
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on", PORT));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/cart", cartRoutes);
+app.use("/api/admin", require("./routes/adminRoutes"));
